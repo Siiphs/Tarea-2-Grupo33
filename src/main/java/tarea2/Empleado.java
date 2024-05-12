dc0aae77878687fca3463b0b4560316c328a435c
hooooooooola <167253637+hooooooooola@users.noreply.github.com>
2024-05-12 02:37:23 +0000

    fix: Añadir setters, getters y toString a Empleado

--- src/main/java/tarea2/Empleado.java
+++ src/main/java/tarea2/Empleado.java
@@ -1,17 +1,64 @@
-package tarea2;
+package tarea2;;
 
-public class Empleado implements Invitable {
-    private String id;
-    private String apellidos;
+public class Empleado {
+    private int id;
     private String nombre;
+    private String apellido;
     private String correo;
-    
-    public Empleado(){
+    private Departamento departamento;
 
+    public Empleado(int id, String nombre, String apellido, String correo, Departamento departamento) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.departamento = departamento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
     }
 
     @Override
-    public void invitar(){
-        
+    public void invitar {
+
     }
 }
+
+
